2052273343ab2ca6c3b23454f8a6eb6e65ebfbbb
David Danilchik <davdd@mac.com>
2023-09-27 23:25:45 -0700

    worked on getting featured tasks completed.

--- auth-server/src/server.js
+++ auth-server/src/server.js
@@ -3,38 +3,24 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const errorHandler = require('./error-handlers/500.js');
+const notFound = require('./error-handlers/404.js');
+const authRoutes = require('./auth/routes/routes.js');
 
-const notFoundHandler = require('./error-handlers/404.js');
 const logger = require('./middleware/logger.js');
 
-const v1Routes = require('./routes/v1.js');
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
 app.use(logger);
 
-app.use('/api/v1', v1Routes);
+app.use('/api/v1', authRoutes);
 
-app.use('*', notFoundHandler);
+app.use('*', notFound);
 
-app.use(errorHandler);
-
-// Esoteric Resources
-const errorHandler = require('./error-handlers/500.js');
-const notFound = require('./error-handlers/404.js');
-const authRoutes = require('./auth/routes/routes.js');
-
-// App Level MW
-app.use(cors());
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.use(authRoutes);
-
-// Catchalls
-app.use(notFound);
 app.use(errorHandler);
 
 module.exports = {
