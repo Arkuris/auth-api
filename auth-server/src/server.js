80b9126dc87aa105da354e45130b19c78224f56e
David Danilchik <davdd@mac.com>
2023-09-27 22:47:47 -0700

    combined files and cleaned up

--- auth-server/src/server.js
+++ auth-server/src/server.js
@@ -1,17 +1,28 @@
 'use strict';
 
-// 3rd Party Resources
 const express = require('express');
+const app = express();
 const cors = require('cors');
 
+const notFoundHandler = require('./error-handlers/404.js');
+const logger = require('./middleware/logger.js');
+
+const v1Routes = require('./routes/v1.js');
+
+app.use(express.json());
+
+app.use(logger);
+
+app.use('/api/v1', v1Routes);
+
+app.use('*', notFoundHandler);
+
+app.use(errorHandler);
 
 // Esoteric Resources
 const errorHandler = require('./error-handlers/500.js');
 const notFound = require('./error-handlers/404.js');
-const authRoutes = require('./auth/routes.js');
-
-// Prepare the express app
-const app = express();
+const authRoutes = require('./auth/routes/routes.js');
 
 // App Level MW
 app.use(cors());
